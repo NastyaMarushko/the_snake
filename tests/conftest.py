b866f2975a5fb88d1fb50c3543c6b75db83d6258
Nastya Marushko <marushkoasya@yandex.ru>
2026-05-30 18:31:31 +0300

    Исправлен импорт pygame на pygame_ce для работы с установленной библиотекой

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from multiprocessing import Process
 from pathlib import Path
 from typing import Any
 
-from pygame.time import Clock
+from pygame_ce.time import Clock
 import pytest
 import pytest_timeout
 
